33c21cb89d05777606909686f842ca540bc5528c
LShaggyo <cguzmanabrigo@Carloss-MacBook-Pro.local>
2023-06-09 18:59:14 -0400

    Barra nav responsiva terminada

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+let showMenu = false; // Declaro variable p
+
+function showHiddenMenu(){ // Funcion para ocultar y mostrar menu
+    if (showMenu) {
+        document.getElementById("hidden-nav").classList = ""; // Crea la clase "" 
+        showMenu = false;
+    } else {
+        document.getElementById("hidden-nav").classList = "show-nav"; // Crea la clase "show-menu" 
+        showMenu = true; 
+    }
+}
+
+function selection(){ // Funcion oculta menu al seleccionar una opcion
+    document.getElementById("hidden.nav").classList = "";
+    showMenu = false;
+}
